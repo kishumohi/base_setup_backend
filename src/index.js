a03c1dbdd75300e34806a91f121ba1287990b868
Kishan Kalavadiya <36093612+kishumohi@users.noreply.github.com>
2025-01-22 16:44:40 +0530

    sequelize reference function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { ServerConfig } = require("./config");
 const apiRoutes = require("./routes");
+const { where } = require("sequelize");
 
 const app = express();
 
@@ -8,6 +9,25 @@ app.use(express.json({}));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
   console.log(`Sever running on PORT : ${ServerConfig.PORT}`);
+  const { City, Airport } = require("./models");
+  // const ahmedabad = await City.findByPk(2);
+  // console.log(ahmedabad);
+
+  // const sivajiAirport = await ahmedabad.createAirport({
+  //   name: "Mumbai Airport",
+  //   code: "MAR",
+  // });
+
+  // const airportsInSAR = await ahmedabad.getAirports();
+  // console.log(airportsInSAR);
+
+  // console.log(sivajiAirport);
+
+  await City.destroy({
+    where: {
+      id: 1,
+    },
+  });
 });
